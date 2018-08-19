d63b52a2d24634440f4a504a380e21bd2db1f539
rolling <galixig3n@gmail.com>
2018-08-19 21:53:47 +0300

    Order Bill

---
+++ src/components/molecules/OrderBill.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import OrderBillRow from '../atoms/OrderBillRow';
+
+const OrderBill = ({ className, values }) => (
+  <section className={className}>
+    {values.map(([title, value]) => (
+      <OrderBillRow title={title} value={value} />
+    ))}
+  </section>
+);
+
+export default styled(OrderBill)`
+  border: 1px solid inherit;
+  border-width: 1px 0;
+
+  padding: 15px 0;
+
+  ${OrderBillRow} {
+    margin: 5px 0;
+  }
+`;
