03cbde2f1b237c0a14310dac8de2c254c90e8a94
rolling <galixig3n@gmail.com>
2018-08-19 23:34:36 +0300

    checkout button

---
+++ src/components/atoms/CheckoutButton.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+const Arrow = styled.span`
+  width: 20px;
+  height: 2px;
+  background: white;
+  position: absolute;
+
+  ::after {
+    content: '';
+    border: 2px solid white;
+    border-width: 0 2px 2px 0;
+    position: absolute;
+  }
+`;
+
+const CheckoutButton = ({ className }) => (
+  <button type="submit" className={className}>
+    BUY NOW
+    {' '}
+    <Arrow />
+  </button>
+);
+
+export default styled(CheckoutButton)`
+  background-color: #ff458f;
+  font-size: 18px;
+  box-sizing: border-box;
+  padding: 15px 60px;
+`;
