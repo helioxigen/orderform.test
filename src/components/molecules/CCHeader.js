238a0d28d3efafb5df781930616898ec35002e59
rolling <galixig3n@gmail.com>
2018-08-19 23:24:26 +0300

    CC Header

---
+++ src/components/molecules/CCHeader.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import CCSafetyMessage from '../atoms/CCSafetyMessage';
+import CCardType from '../atoms/CCardType';
+
+const CCHeader = ({ className }) => (
+  <header className={className}>
+    <CCSafetyMessage />
+    <div className="cards">
+      {['visa', 'master', 'maestro', 'american'].map(type => (
+        <CCardType type={type} />
+      ))}
+    </div>
+  </header>
+);
+
+export default styled(CCHeader)`
+  .cards {
+    ${CCardType} {
+      margin: 0 5px;
+    }
+  }
+`;
