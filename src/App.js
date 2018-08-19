d4131101b97ab734ad95658c2c4b1509a6669453
rolling <galixig3n@gmail.com>
2018-08-19 23:22:31 +0300

    App layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-export default () => (
-  <div>
-    <div />
+import PageLayout from 'components/template/PageLayout';
+import Form from 'components/organisms/Form';
+import Order from 'components/organisms/Order';
+import styled from 'styled-components';
+import LogoHeader from 'components/atoms/LogoHeader';
+
+const App = ({ className }) => (
+  <div className={className}>
+    <LogoHeader />
+    <PageLayout>
+      <Order />
+      <Form />
+    </PageLayout>
   </div>
 );
+
+export default styled(App)`
+  padding: 20px 120px 0;
+`;
